5c4bf5bc42b424c98cacf8885b4cb538fb424ce8
FernandoDanielZigarra <fernando.zigarra@gmail.com>
2022-03-20 02:41:13 -0300

    MaquetacionFinalizada

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path')
 const app = express();
 const port = 3030;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname ,'public')));
 
 app.get('/', (req,res) => {
-    return res.sendFile(path.resolve(__dirname,'views','home.html'))
+    res.sendFile(path.resolve(__dirname,'views','home.html'))
 });
 
 app.listen(port, () => console.log(`Server running on port http://localhost:${port}`))
